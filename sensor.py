8f3371e0ca09f4db4b9b18d5bf2f51ca076fa26d
Katie Baldwin <kgb2@princeton.edu>
2023-03-10 15:41:07 -0500

    raw data wakeup

--- sensor.py
+++ sensor.py
@@ -36,15 +36,20 @@ class SensorNode:
 
         # maps variable name to list storing current cache of data and the next index to be filled
         self.data = {}
+        self.min_interval = np.inf
         for item in self.variables:
             self.data[item] = [np.zeros(self.variables[item].histlen), 0]
+            if self.variables[item].histlen < self.min_interval:
+                self.min_interval = self.variables[item].histlen
+
+        self.since_last_sent = 0
 
         self.timer1 = task.LoopingCall(self._sleep)
         self.timer1.start(1)
 
         # this should execute as often as the sensor is to be woken up
-        self.timer2 = task.LoopingCall(self.wakeup)
-        self.timer2.start(5)
+        self.timer2 = task.LoopingCall(self.raw_data_wakeup)
+        self.timer2.start(3)
 
         reactor.run()
 
@@ -81,8 +86,46 @@ class SensorNode:
         self.timer1.start(1)  # re-enters sleep mode
 
 
+    def raw_data_wakeup(self):
+        print("waking up")
+        self.timer1.stop()
+
+        self.energy_level -= 3
+
+        # take measurements
+        for item in self.variables:
+            raw_value = self.variables[item].get_measurement()
+            data_r = self.data[item]
+            data_r[0][data_r[1]] = raw_value
+            data_r[1] = (data_r[1] + 1) % self.variables[item].histlen
+
+            self.energy_level -= self.variables[item].energyUsage
+
+        # check if it's time to send data
+        self.since_last_sent += 1
+        if self.since_last_sent == self.min_interval:
+            self.since_last_sent = 0
+
+            # accumulate message containing all data
+            data = ''
+
+            for item in self.variables:
+                data += item + '\n'
+                data_arr = self.data[item][0]
+                for value in data_arr:
+                    data += str(value) + '\n'
+                data += '\n'
+
+            self._send_data(data)
+
+        self.timer1.start(1)  # re-enters sleep mode
+
+
+
     def _send_data(self, data):
         self.energy_level -= 5 # turning radio on/off (not tcp)
+
+        print(data)
 
         #  but I should do this over time b/c it's not sleeping for that time
         self.energy_level -= len(data) * 0.4
@@ -92,7 +135,7 @@ class SensorNode:
         return self.energy_level
 
 
-var1 = Variable(-20, 100, 2, processing.dummy, 8)
-var2 = Variable(0, 5, 3, processing.avg, 8)
+var1 = Variable(-20, 100, 2, processing.dummy, 6)
+var2 = Variable(0, 5, 3, processing.avg, 6)
 variables = {"temp": var1, "light": var2}
 sensor1 = SensorNode(100, variables)
