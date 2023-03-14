8c77e569bcd9dc93c8307657d81f0f85164bf252
Katie Baldwin <kgb2@princeton.edu>
2023-03-14 10:43:29 -0400

    fixing input processing

--- processing.py
+++ processing.py
@@ -2,9 +2,10 @@
 import numpy as np
 
 def occupancy(inputs):
-    print(f"calculating occupancy using temp input \n{inputs['temp']}\n and light input \n{inputs['light']}")
+    print(f'''calculating occupancy using temp input \n{inputs['temp']}
+      and light input \n{inputs['light']}''')
 
 def tempAvg(inputs):
-    output = np.average(input[0])
+    output = np.average(inputs["temp"])
     print("Average: " + str(output))
     return output
