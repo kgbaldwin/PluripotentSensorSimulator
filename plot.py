962f615ef73085da7eb572b50e2bbb631070c01f
Katie Baldwin <kgb2@princeton.edu>
2023-03-16 17:24:18 -0400

    plotting energy usage

---
+++ plot.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = open(sys.argv[1])
+
+values = [round(float(f), 2) for f in file.readlines()]
+
+diff = [round(values[x+1]-values[x], 2) for x in range(len(values)-1)]
+print(diff)
+
+x = np.arange(0, len(values))
+print()
+print(values)
+
+plt.plot(x, values)
+plt.show()
