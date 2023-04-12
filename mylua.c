d6dfd8840a7fe9654baa5f8ea4a8fb0b3691647c
Katie Baldwin <kgb2@princeton.edu>
2023-04-11 23:06:43 -0400

    mylua

--- mylua.c
+++ mylua.c
@@ -20,22 +20,12 @@ int main (int argc, char *argv[]) {
 
     lua_sethook(L, *instr_count, LUA_MASKCOUNT, 1);
 
-    luaL_dofile(L, argv[1]);
-
-    /*
-    while (fgets(buff, sizeof(buff), stdin) != NULL) {
-        /* fprintf(stderr, "D:%s\n", buff);
-        error = luaL_loadbuffer(L, buff, strlen(buff)-1, "line");
-        if (error) {
-            fprintf(stderr, "E:%s\n", lua_tostring(L, -1));
-            lua_pop(L, 1);  /* pop error message from the stack
-        }
-    }
-    error = lua_pcall(L, 0, 0, 0);
+    error = luaL_dofile(L, argv[1]);
     if (error) {
-        fprintf(stderr, "F:%s\n", lua_tostring(L, -1));
-        lua_pop(L, 1);  /* pop error message from the stack
-    }*/
+        fprintf(stderr, "E:%s\n", lua_tostring(L, -1));
+        lua_pop(L, 1);  /* pop error message from the stack */
+    }
+
     printf("Instruction count: %d\n", instructions);
 
     lua_close(L);
