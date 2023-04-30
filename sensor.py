7fafee5f86942dee64a7fc959e473ab77b63ea4c
Katie Baldwin <kgb2@princeton.edu>
2023-04-29 21:27:31 -0400

    cleaning up code, adding mips

--- sensor.py
+++ sensor.py
@@ -16,7 +16,7 @@ class Variable:
         self.rng = rangeMax - rangeMin  # range of possible output values for a measurement
         self.rangeMin = rangeMin   # min of possible output values for a measurement
 
-        self.freq = frequency   # frequency at which this variable is measured
+        self.freq = frequency   # frequency at which this variable is measured (seconds)
 
     def get_measurement(self):
         return random.random() * self.rng + self.rangeMin
@@ -32,7 +32,7 @@ class ComputeFunction:
 class SensorNode:
 
     def __init__(self, variables: dict, functions: dict, parameters: dict, raw: bool, cycles: int):
-        self.energy_level = parameters["Energy"] # given in "mAs"
+        self.energy_level = parameters["Energy"]   # given in "mAs"
         print("inital energy level")
         print(self.energy_level, "0")
         self.curr_t = 0
@@ -50,13 +50,13 @@ class SensorNode:
         cycle_max = 1
         min_freq = np.inf
 
-        # freqencies of variables
+        # frequencies of variables
         for name, item in self.variables.items():
             self.raw_data[name] = [np.zeros(parameters["Raw_cache"]), 0]
             if min_freq > item.freq:
                 min_freq = item.freq
 
-            # undetermined behavior when frequencies aren't a multiple of wakeup_freq
+            # undetermined behavior when frequencies aren't a multiple of Wakeup_freq
             num = int(item.freq // parameters["Wakeup_freq"])
             if cycle_max % num != 0:
                 cycle_max = math.lcm(cycle_max, num)
@@ -84,18 +84,20 @@ class SensorNode:
         self.run_loop(raw)
 
 
-    # Simulates lifetime-loop of sensor, calling other functions in the
-    # appropriate sequence
+    # Simulates lifetime-loop of sensor, calling other functions in
+    # their appropriate sequence
     def run_loop(self, raw):
 
         p = self.parameters
 
+        # loop enough times such that data is sent num_cycles number of times
         for _ in range(self.num_cycles * self.send_freq):
 
+            # terminate if sensor's battery has been depleted
             if self.energy_level < 0:
                 return
 
-            # estimating this takes ~1/6 s
+            # estimating that wakeup takes ~1/6 s
             self.energy_level -= p["Wakeup_E"]/6
             self.curr_t += 1/6
 
@@ -116,11 +118,70 @@ class SensorNode:
 
             self.curr_t += time_to_sleep
 
-            # record energy draw during this cycle
+            # record energy draw during this sleep cycle
             print("after sleeping")
             print(round(self.energy_level, 4), round(self.curr_t, 3))
 
+        # after simulation has completed, print lifetime stats
         self.lifetime_stats()
+
+
+
+    # Measures all variables to be measured at current timestep
+    def get_measurements(self):
+
+        b = self.parameters["Raw_cache"]
+        f = self.parameters["Wakeup_freq"]
+
+        for name, var in self.variables.items():
+
+            # if variable is to be measured this cycle
+            if self.cycle%(var.freq // f) == 0:
+
+                raw_value = var.get_measurement()
+                data_r = self.raw_data[name]
+
+                # data_r[0] is data array; data_r[1] is its next index to be filled
+                data_r[0][data_r[1]] = raw_value
+                data_r[1] = (data_r[1] + 1) % b
+
+                self.energy_level -= var.energyUsage
+
+        self.cycle = (self.cycle + 1) % self.cycle_max
+
+        # increment time by 1 second per variable measured
+        self.curr_t += len(self.variables)
+
+        print("after getting measurements")
+        print(round(self.energy_level, 4), round(self.curr_t, 3))
+
+
+
+    # Simulates sensor wakeup where it only collects raw data and sends
+    # back to access point if necessary
+    def raw_data_wakeup(self):
+
+        # check if it's time to send data
+        self.since_last_sent += 1
+        if self.since_last_sent == self.send_freq:
+            self.since_last_sent = 0
+
+            # count total number of bytes to be sent
+            num_bytes = 0
+            for item in self.variables:
+                num_bytes += 1    # variable identifier byte
+
+                data_arr = self.raw_data[item][0]
+                # number of raw values that cache contains
+                num_values = self.raw_data[item][1] + 1
+                num_bytes += 4 * num_values   # 4 bytes per float
+
+                # reset array
+                for i in range(num_values):
+                    data_arr[i] = 0
+                self.raw_data[item][1] = 0  # reset current index
+
+            self.send_data(num_bytes)
 
 
     # Simulates sensor wakeup where it performs computations
@@ -133,10 +194,11 @@ class SensorNode:
 
                 self.write_files(func)
 
-                # first, count how many instructions line-retrieving for function takes
+                # count number of instructions for line-retrieving for function
                 if funcObj.load_inst == -1:
                     self.count_load_instructions(func)
 
+                # run function
                 executable = os.getcwd() + '/mylua'
                 output = subprocess.run([executable, 'luaRunner.lua'], capture_output=True)
                 result = output.stdout.decode()
@@ -145,16 +207,17 @@ class SensorNode:
                 instructions = int(result.split()[3]) - funcObj.load_inst
 
                 # subtract function energy
-                self.energy_level -= self.parameters["Processor_E"] * instructions / 10000
+                ips = self.parameters["MIPS"] * 1000000
+                self.energy_level -= self.parameters["Processor_E"] * instructions / ips
                 self.curr_t += 1   ## assume 1 second for function execution
                 print("after executing function")
                 print(round(self.energy_level, 4), round(self.curr_t, 3))
 
-                data_c = self.computed_data[func]
-
                 # data_c[0] is data array; data_c[1] is its next index to be filled
+                data_c = self.computed_data[func]
                 data_c[0][data_c[1]] = float(result.split()[0])
                 data_c[1] = (data_c[1] + 1) % len(data_c[0])
+
 
         self.since_last_sent += 1
         if self.since_last_sent == self.send_freq:
@@ -182,8 +245,6 @@ class SensorNode:
     # writes lua runner file and data transfer file, given a function
     def write_files(self, func):
 
-        input_vars = self.functions[func].inputs
-
         # write data to file to send to lua
         file = open("input.txt", "w")
 
@@ -195,6 +256,7 @@ class SensorNode:
         luaRunner.write("print(lib." + func + "(")
 
         # input_vars: vars to be inputted to function
+        input_vars = self.functions[func].inputs
         for counter, var in enumerate(input_vars):
             datapoints = self.raw_data[var][0]
 
@@ -230,66 +292,13 @@ class SensorNode:
         self.functions[func].load_inst = int(result)
 
 
-    # Simulates sensor wakeup where it only collects raw data and sends
-    # back to access point if necessary
-    def raw_data_wakeup(self):
 
-        # check if it's time to send data
-        self.since_last_sent += 1
-        if self.since_last_sent == self.send_freq:
-            self.since_last_sent = 0
-
-            # count total number of bytes to be sent
-            num_bytes = 0
-            for item in self.variables:
-                num_bytes += 1    # variable identifier byte
-
-                data_arr = self.raw_data[item][0]
-                # number of raw values that cache has; 4 bytes per float
-                num_values = self.raw_data[item][1] + 1
-                num_bytes += 4 * num_values
-
-                # reset array
-                for i in range(num_values):
-                    data_arr[i] = 0
-                self.raw_data[item][1] = 0  # reset current index
-
-            self.send_data(num_bytes)
-
-
-    # Measures all variables to be measured at current timestep
-    def get_measurements(self):
-
-        b = self.parameters["Raw_cache"]
-        f = self.parameters["Wakeup_freq"]
-
-        for name, var in self.variables.items():
-
-            if self.cycle%(var.freq // f) == 0:
-
-                raw_value = var.get_measurement()
-                data_r = self.raw_data[name]
-
-                # data_r[0] is data array; data_r[1] is its next index to be filled
-                data_r[0][data_r[1]] = raw_value
-                data_r[1] = (data_r[1] + 1) % b
-
-                self.energy_level -= var.energyUsage
-
-        self.cycle = (self.cycle + 1) % self.cycle_max
-
-        self.curr_t += len(self.variables)
-
-        print("after getting measurements")
-        print(round(self.energy_level, 4), round(self.curr_t, 3))
-
-
-    # Simulates sending data back to access point
+    # Simulates energy of sending data back to access point
     def send_data(self, num_bytes):
 
         p = self.parameters
         self.energy_level -= p["Radio_E"]/6  # turning radio on
-        self.curr_t += 1/6    # time to turn radio on
+        self.curr_t += 1/6    # time to turn radio on (s)
 
         time = 1 + 0.04*num_bytes
         self.curr_t += time
@@ -300,7 +309,8 @@ class SensorNode:
         print(round(self.energy_level, 4), round(self.curr_t, 3))
 
 
-    # after simulation stops, calculates energy used during simulation
+
+    # After simulation stops, calculates energy used during simulation
     # and total expected battery life
     def lifetime_stats(self):
         p = self.parameters
