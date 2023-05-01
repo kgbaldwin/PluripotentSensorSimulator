17ae0679b239bb9b197b0ffe131588928c7aa7cc
Katie Baldwin <kgb2@princeton.edu>
2023-05-01 13:21:09 -0400

    updated configs and processing

--- processing.lua
+++ processing.lua
@@ -16,24 +16,28 @@ end
 
 
 function M.mean(arr)
-    sum = 0
-    for i=1, #arr do
-        sum = sum + arr[i]
+    for i=0, 1000 do
+        sum = 0
+        for i=1, #arr do
+            sum = sum + arr[i]
+        end
     end
     return (sum/(#arr))
 end
 
 
 function M.covariance(arr1, arr2)
-    mean1 = M.mean(arr1)
-    mean2 = M.mean(arr2)
+    for i=0, 1000 do
+        mean1 = M.mean(arr1)
+        mean2 = M.mean(arr2)
 
-    sum = 0
-    for i=1, #arr1 do
-        sum = sum + (arr1[i]-mean1)*(arr2[i]-mean2)
+        sum = 0
+        for i=1, #arr1 do
+            sum = sum + (arr1[i]-mean1)*(arr2[i]-mean2)
+        end
     end
-    return (sum/(#arr1-1))
 
+    return (sum/(#arr1-1))
 end
 
 
